21c734b8058cbe866e9823f128411617491943f2
lucasrafaelmadeiravasconcelos <madeira@rarolabs.com.br>
2019-10-21 22:37:03 -0300

    WIP - Setting up the data access interface for the Domain Layer

---
+++ app/domain/src/main/java/com/example/domain/model/Project.kt
@@ -0,0 +1,7 @@
+package com.example.domain.model
+
+data class Project(
+    val id: String, val name: String, val fullName: String,
+    val starCount: String, val dateCreated: String, val ownerName: String,
+    val ownerAvatar: String, val isBookMaked: Boolean
+)
