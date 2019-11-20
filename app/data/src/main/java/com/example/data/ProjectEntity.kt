c2d1325e00525b1556dca1af638fc3d2808ecb69
lucasrafaelmadeiravasconcelos <madeira@rarolabs.com.br>
2019-11-19 22:23:56 -0300

    Creating the data models for the data layer

---
+++ app/data/src/main/java/com/example/data/ProjectEntity.kt
@@ -0,0 +1,7 @@
+package com.example.data
+
+class ProjectEntity(
+    val id: String, val name: String, val fullName: String,
+    val starCount: String, val dateCreated: String, val ownerName: String,
+    val ownerAvatar: String, val isBookMaked: Boolean
+)
