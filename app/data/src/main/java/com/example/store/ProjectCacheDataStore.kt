1374e9214c5302e5a3ae07bc725ac9ee5e602746
lucasrafaelmadeiravasconcelos <madeira@rarolabs.com.br>
2020-01-16 14:12:07 -0300

    Creating the data layer remote data store

---
+++ app/data/src/main/java/com/example/store/ProjectCacheDataStore.kt
@@ -0,0 +1,39 @@
+package com.example.store
+
+import com.example.data.ProjectEntity
+import com.example.repository.ProjectsCache
+import com.example.repository.ProjectsDataStore
+import io.reactivex.Completable
+import io.reactivex.Observable
+import javax.inject.Inject
+
+class ProjectCacheDataStore @Inject constructor(
+    private val projectsCache: ProjectsCache
+) : ProjectsDataStore {
+
+    override fun getProjects(): Observable<List<ProjectEntity>> {
+        return projectsCache.getProjects()
+    }
+
+    override fun saveProjetc(projects: List<ProjectEntity>): Completable {
+        return projectsCache.saveProjects(projects)
+            .andThen(projectsCache.setLastCacheTime(System.currentTimeMillis()))
+    }
+
+    override fun clearProjects(): Completable {
+        return projectsCache.clearProjects()
+    }
+
+    override fun getBookmarkedProjects(): Observable<List<ProjectEntity>> {
+        return projectsCache.getBookmarkedProjects()
+    }
+
+    override fun setProjectAsBookmarked(projectId: String): Completable {
+        return projectsCache.setProjectAsBookmarked(projectId)
+    }
+
+    override fun setProjectAsNotBookmarked(projectId: String): Completable {
+        return projectsCache.setProjectAsNotBookmarked(projectId)
+    }
+
+}
